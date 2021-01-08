c05444c184421cd15c9cea19b3dbfc6fc709c37f
Grzegorz Stokłosa <gstok@poczta.fm>
2021-01-08 03:01:42 +0100

    Better destination directory

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const getFileName = (url) => url.substring(url.lastIndexOf('-') + 1);
 const downloadImage = async (url) => {
   const response = await fetch(url);
   const buffer = await response.buffer();
-  fs.writeFile(`./download/${getFileName(url)}`, buffer, () => console.log(`Downloaded: ${url}`));
+  fs.writeFile(path.join(__dirname, 'download', getFileName(url)), buffer, () => console.log(`Downloaded: ${url}`));
 };
 
 fs.mkdirSync(path.join(__dirname, 'download'), { recursive: true });
